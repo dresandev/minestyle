1a674cf4fd1f8f9984c0e2a236aee1311d4b1fbc
Dresan <dresan.dev@gmail.com>
2026-03-10 22:01:35 -0500

    chore: upgrade dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,24 +1,25 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import { defineConfig, globalIgnores } from "eslint/config"
+import nextVitals from "eslint-config-next/core-web-vitals"
+import nextTs from "eslint-config-next/typescript"
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-})
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript", "eslint-config-prettier",),
-  ...compat.config({
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
     rules: {
       "semi": ["warn", "never"],
       "jsx-quotes": ["warn", "prefer-double"],
       "quotes": ["warn", "double"],
-      "@next/next/no-img-element": "off"
+      "@next/next/no-img-element": "off",
+      "react-hooks/set-state-in-effect": "off"
     }
-  })
-]
+  },
+  globalIgnores([
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+])
 
 export default eslintConfig
